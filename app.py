feebfe6aaaa430509bd04d663ec1b0cf6f989d7a
Evgeny Gladkih <eogladkih@hotmaul.com>
2021-01-27 21:01:43 +0300

    add templates with data and creat base.html template

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+from data import title, subtitle, description, departures, tours
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', title=title)
+
+
+@app.route('/departures/<departure>/')
+def dep(departure):
+    return render_template('departure.html', title=title)
+
+
+@app.route('/tours/<int:id>/')
+def tour(id):
+    return render_template('tour.html', title=title)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
